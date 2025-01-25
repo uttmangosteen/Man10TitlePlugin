ebe75f0b564efc7f03363e46521dc0254520dd7f
Underthetree <noisimportantpoint@gmail.com>
2025-01-25 20:34:30 +0900

    Initial commit

---
+++ src/main/java/io/github/uttmangosteen/man10TitlePlugin/MTitle.java
@@ -0,0 +1,39 @@
+package io.github.uttmangosteen.man10TitlePlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class MTitle implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String alias, String[] args) {
+        if (!(sender.hasPermission("red.man10.mtitle"))) return true;
+        String title, subtitle = "";
+        int stay = 5, fadeOut = 2;
+        switch (args.length) {
+            case 4:
+                try {
+                    fadeOut = Integer.parseInt(args[3]);
+                } catch (NumberFormatException ignored) {}
+            case 3:
+                try {
+                    stay = Integer.parseInt(args[2]);
+                } catch (NumberFormatException ignored) {}
+            case 2:
+                subtitle = Objects.equals(args[1], "-") ? "" : args[1].replace('&', '§');
+            case 1:
+                title = Objects.equals(args[0], "-") ? "" : args[0].replace('&', '§');
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    p.sendTitle(title, subtitle, 0, stay * 20, fadeOut * 20);
+                    p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 1);
+                }
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/io/github/uttmangosteen/man10TitlePlugin/Main.java
@@ -0,0 +1,14 @@
+package io.github.uttmangosteen.man10TitlePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Objects;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Objects.requireNonNull(getCommand("mtitle")).setExecutor(new MTitle());
+        Objects.requireNonNull(getCommand("mtitle_test")).setExecutor(new MTitleTest());
+        Objects.requireNonNull(getCommand("mtitle_all")).setExecutor(new MTitleAll());
+    }
+}
