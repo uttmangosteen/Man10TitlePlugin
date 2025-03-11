7cd80d8f0d865effa5293525dc14a9718f79bc0d
Underthetree <noisimportantpoint@gmail.com>
2025-03-11 09:52:10 +0900

    aaaa

--- src/main/java/io/github/uttmangosteen/man10TitlePlugin/MTitle.java
+++ src/main/java/io/github/uttmangosteen/man10TitlePlugin/MTitle.java
@@ -1,44 +1,49 @@
 package io.github.uttmangosteen.man10TitlePlugin;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 public class MTitle implements CommandExecutor {
+    private final JavaPlugin plugin;
+
+    public MTitle(JavaPlugin plugin) {this.plugin = plugin;}
     @Override
     public boolean onCommand(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String alias, String[] args) {
         if (!(sender.hasPermission("red.man10.mtitle"))) return true;
         if (!(sender instanceof Player) && alias.equals("mtitle_test")) return true;
         if (args.length == 0) return false;
+
         StringBuilder title = new StringBuilder();
         StringBuilder subtitle = new StringBuilder();
         int stay = 5, fadeIn = 1, fadeOut = 1;
         for (int i = 0, j = 0; i < args.length; i++) {
             if (args[i].equals("|")) {j++;continue;}
-            args[i] = args[i].replace("&", "§");
-            args[i] = args[i].replace("#", " ");
+            args[i] = args[i].replace("&", "§").replace("#", " ");
             switch (j) {
                 case 0: title.append(args[i]);continue;
                 case 1: subtitle.append(args[i]);continue;
-                case 2:
-                    try {stay = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}
-                    continue;
-                case 3:
-                    try {fadeIn = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}
-                    continue;
-                case 4:
-                    try {fadeOut = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}
+                case 2: try {stay = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}continue;
+                case 3: try {fadeIn = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}continue;
+                case 4: try {fadeOut = Integer.parseInt(args[i]);} catch (NumberFormatException ignored) {}
+                default: break;
             }
         }
+
         if (Objects.equals(alias, "mtitle_test")) {
             Player p = (Player) sender;
             p.sendTitle(title.toString(), subtitle.toString(), fadeIn * 20, stay * 20, fadeOut * 20);
             p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 1);
+        } else if (Objects.equals(alias, "mtitle_all")) {
+
         } else {
             for (Player p : Bukkit.getOnlinePlayers()) {
                 p.sendTitle(title.toString(), subtitle.toString(), fadeIn * 20, stay * 20, fadeOut * 20);
